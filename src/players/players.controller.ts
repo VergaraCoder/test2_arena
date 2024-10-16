3383f8d50af812f4bc586d00d79c736838b73fa8
vergaraCoder <jhonatanvergara072@gmail.com>
2024-10-16 16:49:55 -0400

    feat: creation of methods of players

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -8,27 +8,27 @@ export class PlayersController {
   constructor(private readonly playersService: PlayersService) {}
 
   @Post()
-  create(@Body() createPlayerDto: CreatePlayerDto) {
+  async create(@Body() createPlayerDto: CreatePlayerDto) {
     return this.playersService.create(createPlayerDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.playersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     return this.playersService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePlayerDto: UpdatePlayerDto) {
+  async update(@Param('id') id: string, @Body() updatePlayerDto: UpdatePlayerDto) {
     return this.playersService.update(+id, updatePlayerDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string) {
     return this.playersService.remove(+id);
   }
 }
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,26 +1,85 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePlayerDto } from './dto/create-player.dto';
 import { UpdatePlayerDto } from './dto/update-player.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Player } from './entities/player.entity';
+import { Repository } from 'typeorm';
+import { manageErrors } from 'src/common/errors/custom/err.custom';
 
 @Injectable()
 export class PlayersService {
-  create(createPlayerDto: CreatePlayerDto) {
-    return 'This action adds a new player';
+  constructor(
+    @InjectRepository(Player)
+    private playerRepository: Repository<Player>,
+  ) {}
+  async create(createPlayerDto: CreatePlayerDto) {
+    const dataPlayer = this.playerRepository.create(createPlayerDto);
+    await this.playerRepository.save(dataPlayer);
+    return dataPlayer;
   }
 
-  findAll() {
-    return `This action returns all players`;
+  async findAll() {
+    try {
+      const find = await this.playerRepository.find();
+      if (find.length == 0) {
+        throw new manageErrors({
+          type: 'NOT_FOUND',
+          message: 'DOES THERE ARE NOT PLAYERS .',
+        });
+      }
+      return find;
+    } catch (err: any) {
+      throw new manageErrors.signatureError(err.message);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} player`;
+  async findOne(id: number) {
+    try {
+      const findOne = await this.playerRepository.findOne({
+        where: { id: id },
+      });
+      if (!findOne) {
+        throw new manageErrors({
+          type: 'NOT_FOUND',
+          message: 'THAT ID PLAYER NOT FOUND .',
+        });
+      }
+      return findOne;
+    } catch (err: any) {
+      throw new manageErrors.signatureError(err.message);
+    }
   }
 
-  update(id: number, updatePlayerDto: UpdatePlayerDto) {
-    return `This action updates a #${id} player`;
+  async update(id: number, updatePlayerDto: UpdatePlayerDto) {
+    try {
+      const { affected } = await this.playerRepository.update(
+        id,
+        updatePlayerDto,
+      );
+      if (affected == 0) {
+        throw new manageErrors({
+          type: 'NOT_FOUND',
+          message: 'FAILED TO UPDATE .',
+        });
+      }
+      return 'PERFECTLY UPDATED .';
+    } catch (err: any) {
+      throw new manageErrors.signatureError(err.message);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} player`;
+  async remove(id: number) {
+    try {
+      const { affected } = await this.playerRepository.delete(id);
+      if (affected == 0) {
+        throw new manageErrors({
+          type: 'NOT_FOUND',
+          message: 'FAILTED TO DELETED .',
+        });
+      }
+      return 'Perfectly deleted .';
+    } catch (err: any) {
+      throw new manageErrors.signatureError(err.message);
+    }
   }
 }
